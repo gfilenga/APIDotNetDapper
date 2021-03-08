92e4db35acac2c8ce1320c729c99215ec4d9d9f3
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2021-03-08 00:06:24 -0300

    Definindo Entidades

---
+++ BaltaStore.Domain/StoreContext/Customer.cs
@@ -0,0 +1,15 @@
+
+using System;
+
+namespace BaltaStore.Domain.StoreContext
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
