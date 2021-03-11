4ff129d80e1f001b92e59299b76c38de1578af56
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2021-03-11 18:08:13 -0300

    FailFastValidations no CreateCustomerCommand

--- BaltaStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
+++ BaltaStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
@@ -1,6 +1,9 @@
+using FluentValidator;
+using FluentValidator.Validation;
+
 namespace BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Inputs
 {
-    public class CreateCustomerCommand
+    public class CreateCustomerCommand : Notifiable
     {
         public string FirstName { get; set; }
 
@@ -11,5 +14,21 @@ namespace BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Inputs
         public string Email { get; set; }
 
         public string Phone { get; set; }
+
+
+        // Fail Fast Validation
+        public bool Valid()
+        {
+            AddNotifications(new ValidationContract()
+                .HasMinLen(FirstName, 2, "FirstName", "O nome deve conter pelo menos 2 caracteres")
+                .HasMaxLen(FirstName, 40, "FirstName", "O nome deve conter no máximo 40 caracteres")
+                .HasMinLen(LastName, 2, "LastName", "O sobrenome deve conter pelo menos 2 caracteres")
+                .HasMaxLen(LastName, 40, "LastName", "O sobrenome deve conter no máximo 40 caracteres")
+                .IsEmail(Email, "Email", "O email é inválido")
+                .HasLen(Document, 11, "Document", "CPF Inválido")
+            );
+
+            return Valid();
+        }
     }
 }
