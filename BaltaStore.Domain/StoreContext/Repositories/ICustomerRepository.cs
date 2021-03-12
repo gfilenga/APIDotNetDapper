2fe38c25d7a17211ca6fe7c77430615a27126357
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2021-03-12 17:24:45 -0300

    Criando repositório

--- BaltaStore.Domain/StoreContext/Repositories/ICustomerRepository.cs
+++ BaltaStore.Domain/StoreContext/Repositories/ICustomerRepository.cs
@@ -5,7 +5,7 @@ namespace BaltaStore.Domain.StoreContext.Repositories
     public interface ICustomerRepository
     {
         bool CheckDocument(string document);
-        bool CheckEmail(string document);
+        bool CheckEmail(string email);
         void Save(Customer customer);
     }
 }
