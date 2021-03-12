71e8d140d63a6f2d77b65e3e3752394397062430
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2021-03-12 19:35:41 -0300

    Criando home controller e configurando startup

--- BaltaStore.Api/Startup.cs
+++ BaltaStore.Api/Startup.cs
@@ -12,7 +12,7 @@ namespace BaltaStore.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,8 +23,16 @@ namespace BaltaStore.Api
                 app.UseDeveloperExceptionPage();
             }
 
+
             app.UseRouting();
             app.UseMvc();
+            // app.UseEndpoints(endpoints =>
+            // {
+            //     endpoints.MapGet("/", async context =>
+            //     {
+            //         await context.Response.WriteAsync("Hello World!");
+            //     });
+            // });
         }
     }
 }
