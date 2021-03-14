240c101627223d9e5d1ccac201887716dcf39778
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2021-03-14 05:56:47 -0300

    Injecao de dependencias

--- BaltaStore.Api/Startup.cs
+++ BaltaStore.Api/Startup.cs
@@ -1,3 +1,8 @@
+using BaltaStore.Domain.StoreContext.Repositories;
+using BaltaStore.Domain.StoreContext.Services;
+using BaltaStore.Infra.StoreContext.DataContexts;
+using BaltaStore.Infra.StoreContext.Repositories;
+using BaltaStore.Infra.StoreContext.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -13,6 +18,10 @@ namespace BaltaStore.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(options => options.EnableEndpointRouting = false);
+
+            services.AddScoped<BaltaDataContext, BaltaDataContext>();
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<IEmailService, EmailService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
